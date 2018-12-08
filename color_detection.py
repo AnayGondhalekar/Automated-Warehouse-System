66d9f1d64875564c78d7f1332a894716ba2ec02d
NikhilDivekar7 <31787984+NikhilDivekar7@users.noreply.github.com>
2018-12-08 16:13:31 -0700

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -2,6 +2,8 @@ import boto3
 
 import time
 import picamera
+import pika
+import logging
 
 import usb.core, usb.util, time
  
@@ -38,6 +40,8 @@ while True:
         a = '{0}'.format(message.body)
         b= str(a.split('"')[3])
         print(b)
+        result = [x.strip() for x in b.split(',')]
+        print(result)
         message.delete()
         flag = 1
 
@@ -48,6 +52,23 @@ while True:
             # Camera warm-up time
             #time.sleep(0.2)
             camera.capture('demo1.jpg')
+            
+        f= open("demo1.jpg","rb")
+        i= f.read()
+
+        logging.basicConfig()
+        credentials = pika.PlainCredentials('the_user','the_pass')
+        connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.180',5672,'/',socket_timeout=15,credentials= credentials))
+        channel = connection.channel()
+
+
+        channel.queue_declare(queue='hello')
+
+        channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body=i)
+        print(" [x] Sent 'Hello World!'")
+        connection.close()
             
         position_top = []
         position_left = []
@@ -69,6 +90,7 @@ while True:
         textDetections=response['TextDetections']
         i = 0
         print("Here")
+        detected_flag = 0
         #print(response)
         #print('Matching faces')
         for text in textDetections:
@@ -77,6 +99,7 @@ while True:
             print('Geometry: ' + str(text['Geometry']['BoundingBox']))
             print('Id: {}'.format(text['Id']))
             
+            #for x in result:
             if(str(text['DetectedText']) is b):
                    print( b + " received")
                    print("position of" + b + "is" + " " + str(text['Geometry']['BoundingBox']['Top']) + " " +str(text['Geometry']['BoundingBox']['Left']))
@@ -84,66 +107,92 @@ while True:
                    print(position_top[0])
                    position_left.append(float(str(text['Geometry']['BoundingBox']['Left'])))
                    print(position_left[0])
+                   detected_flag = 1
             i = i+1
+        
+        if(detected_flag == 1):
+            if(0.23 < position_top[0]):
+                if(position_top[0] < 0.27):    
+                    if(0.35 < position_left[0]):
+                        if(position_left[0] < 0.39):
+                            MoveArm(1.55,[0,1,0]) #Rotate base right
+                            time.sleep(1)
+                            MoveArm(2.30,[128,0,0]) #Shoulder down
+                            time.sleep(1)
+                            MoveArm(1.1,[1,0,0]) #Grip close
+                            time.sleep(1)
+                            MoveArm(3.5,[64,0,0]) #Shoulder up
+                            time.sleep(1)
+                            MoveArm(1.52,[0,2,0]) #Rotate base left
+                            time.sleep(1)
+                            MoveArm(1,[2,0,0]) #Grip open
+                            time.sleep(1)
+            else:
+                print("Not at proper state")
+                
+            if(0.39 < position_top[0]):
+                if(position_top[0] < 0.44):    
+                    if(0.52 < position_left[0]):
+                        if(position_left[0] < 0.56):
+                            print("Move to 2")
+                            MoveArm(2.8,[0,1,0]) #Rotate base right
+                            time.sleep(1)
+                            MoveArm(2.30,[128,0,0]) #Shoulder down
+                            time.sleep(1)
+                            MoveArm(1.1,[1,0,0]) #Grip close
+                            time.sleep(1)
+                            MoveArm(3.5,[64,0,0]) #Shoulder up
+                            time.sleep(1)
+                            MoveArm(2.8,[0,2,0]) #Rotate base left
+                            time.sleep(1)
+                            MoveArm(1,[2,0,0]) #Grip open
+                            time.sleep(1)
+            else:
+                print("Not at proper state")
+                
+            if(0.64 < position_top[0]):
+                if(position_top[0] < 0.70):    
+                    if(0.64 < position_left[0]):
+                        if(position_left[0] < 0.70):
+                            print("Move to 3")
+                            MoveArm(4,[0,1,0]) #Rotate base right
+                            time.sleep(1)
+                            MoveArm(2.30,[128,0,0]) #Shoulder down
+                            time.sleep(1)
+                            MoveArm(1.1,[1,0,0]) #Grip close
+                            time.sleep(1)
+                            MoveArm(3.5,[64,0,0]) #Shoulder up
+                            time.sleep(1)
+                            MoveArm(3.95,[0,2,0]) #Rotate base left
+                            time.sleep(1)
+                            MoveArm(1,[2,0,0]) #Grip open
+                            time.sleep(1)
+            else:
+                print("Not at proper state")
             
-        if(0.23 < position_top[0]):
-            if(position_top[0] < 0.27):    
-                if(0.35 < position_left[0]):
-                    if(position_left[0] < 0.39):
-                        MoveArm(1.55,[0,1,0]) #Rotate base right
-                        time.sleep(1)
-                        MoveArm(2.30,[128,0,0]) #Shoulder down
-                        time.sleep(1)
-                        MoveArm(1.1,[1,0,0]) #Grip close
-                        time.sleep(1)
-                        MoveArm(3.5,[64,0,0]) #Shoulder up
-                        time.sleep(1)
-                        MoveArm(1.52,[0,2,0]) #Rotate base left
-                        time.sleep(1)
-                        MoveArm(1,[2,0,0]) #Grip open
-                        time.sleep(1)
-        else:
-            print("Not at proper state")
+        with picamera.PiCamera() as camera:
+            camera.resolution = (1024, 768)
+            camera.start_preview()
+            # Camera warm-up time
+            #time.sleep(0.2)
+            camera.capture('demo1.jpg')
             
-        if(0.39 < position_top[0]):
-            if(position_top[0] < 0.44):    
-                if(0.52 < position_left[0]):
-                    if(position_left[0] < 0.56):
-                        print("Move to 2")
-                        MoveArm(2.8,[0,1,0]) #Rotate base right
-                        time.sleep(1)
-                        MoveArm(2.30,[128,0,0]) #Shoulder down
-                        time.sleep(1)
-                        MoveArm(1.1,[1,0,0]) #Grip close
-                        time.sleep(1)
-                        MoveArm(3.5,[64,0,0]) #Shoulder up
-                        time.sleep(1)
-                        MoveArm(2.8,[0,2,0]) #Rotate base left
-                        time.sleep(1)
-                        MoveArm(1,[2,0,0]) #Grip open
-                        time.sleep(1)
-        else:
-            print("Not at proper state")
-            
-        if(0.64 < position_top[0]):
-            if(position_top[0] < 0.70):    
-                if(0.64 < position_left[0]):
-                    if(position_left[0] < 0.70):
-                        print("Move to 3")
-                        MoveArm(4,[0,1,0]) #Rotate base right
-                        time.sleep(1)
-                        MoveArm(2.30,[128,0,0]) #Shoulder down
-                        time.sleep(1)
-                        MoveArm(1.1,[1,0,0]) #Grip close
-                        time.sleep(1)
-                        MoveArm(3.5,[64,0,0]) #Shoulder up
-                        time.sleep(1)
-                        MoveArm(3.95,[0,2,0]) #Rotate base left
-                        time.sleep(1)
-                        MoveArm(1,[2,0,0]) #Grip open
-                        time.sleep(1)
-        else:
-            print("Not at proper state")
+        f= open("demo1.jpg","rb")
+        i= f.read()
+
+        logging.basicConfig()
+        credentials = pika.PlainCredentials('the_user','the_pass')
+        connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.180',5672,'/',socket_timeout=15,credentials= credentials))
+        channel = connection.channel()
+
+
+        channel.queue_declare(queue='hello')
+
+        channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body=i)
+        print(" [x] Sent 'Hello World!'")
+        connection.close()
                 
             
         del position_left[:]
